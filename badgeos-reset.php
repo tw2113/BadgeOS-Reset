60963467a3cc644fe114533e8f56e88c801af354
Michael Beckwith <tw2113@gmail.com>
2014-07-06 03:05:57 -0500

    initial commit

---
+++ badgeos-reset.php
@@ -0,0 +1,287 @@
+<?php
+/**
+ * Plugin Name: BadgeOS Reset Developer Add-On
+ * Plugin URI: http://michaelbox.net
+ * Description: This BadgeOS add-on adds support to reset BadgeOS data for developer purpose
+ * Author: Michae; Beckwith
+ * Version: 1.0.0
+ * Author URI: http://michaelbox.net
+ * License: GNU AGPLv3
+ * License URI: http://www.gnu.org/licenses/agpl-3.0.html
+ * Text Domain: badgeos-reset
+ */
+
+/*
+ * Copyright © 2014 Michael Beckwith
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>;.
+*/
+
+/**
+ * Our main plugin instantiation class
+ *
+ * @since 1.0.0
+ */
+class BadgeOS_Reset {
+
+	/**
+	 * Get everything running.
+	 *
+	 * @since 1.0.0
+	 */
+	function __construct() {
+
+		// Define plugin constants
+		$this->basename       = plugin_basename( __FILE__ );
+		$this->directory_path = plugin_dir_path( __FILE__ );
+		$this->directory_url  = plugins_url( dirname( $this->basename ) );
+
+		// Load translations
+		load_plugin_textdomain( 'badgeos-reset', false, dirname( $this->basename ) . '/languages' );
+
+		add_action( 'admin_notices', array( $this, 'maybe_disable_plugin' ) );
+		add_action( 'badgeos_settings', array( $this, 'reset_settings' ) );
+		add_action( 'admin_init', array( $this, 'handle_reset' ) );
+
+	}
+
+	/**
+	 * Adds additional options to the BadgeOS Settings page
+	 *
+	 * @since 1.0.0
+	 */
+	public function reset_settings() {
+
+	?>
+		<tr>
+			<th scope="row">
+				<?php _e( 'Reset all BadgeOS data: ', 'badgeos-reset' ); ?>
+			</th>
+			<td>
+				<label for="badgeos_reset">
+					<input type="checkbox" name="badgeos_reset" id="badgeos_reset" value="1" />
+					<?php _e( 'WARNING: This will delete ALL your BadgeOS data*', 'badgeos-reset' ); ?>
+				</label>
+					<p><small><?php _e( '*As much as we can accurately detect', 'badgeos-reset' ); ?></small></p>
+			</td>
+		</tr>
+	<?php
+	}
+
+	/**
+	 * Run our methods to delete all of our BadgeOS data, if user has checked to.
+	 *
+	 * @since 1.0.0
+	 */
+	public function handle_reset() {
+
+		if ( empty( $_POST ) || '1' !== $_POST['badgeos_reset'] ) {
+			return;
+		}
+
+		$this->reset_achievement_meta();
+		$this->reset_achievement_types();
+		$this->reset_users();
+		$this->reset_options();
+		$this->reset_p2p();
+	}
+
+	/**
+	 * Run SQL DELETE statement on the User Meta table.
+	 *
+	 * @since 1.0.0
+	 */
+	private function reset_users() {
+		global $wpdb;
+
+		//We have two types of keys stored for a user, so we need to handle both.
+		$badgeos_keys = array( 'credly', '_badgeos' );
+		$sql = "DELETE FROM {$wpdb->usermeta} WHERE meta_key LIKE \"%s\"";
+
+		foreach( $badgeos_keys as $key ) {
+			$wpdb->query(
+				$wpdb->prepare(
+					$sql,
+					$key . '%'
+				)
+			);
+		}
+	}
+
+	/**
+	 * Run SQL statement on the Post Meta table
+	 *
+	 * @since 1.0.0
+	 */
+	private function reset_achievement_meta() {
+		global $wpdb;
+
+		$sql = "DELETE FROM {$wpdb->postmeta} WHERE meta_key LIKE \"%s\"";
+
+		$wpdb->query(
+			$wpdb->prepare(
+				$sql,
+				'%badgeos%'
+			)
+		);
+
+	}
+
+	/**
+	 * Query for our post type posts and associated attachments.
+	 *
+	 * Uses WordPress core API functions for complete deletion of asscoiated data instead of simply deleting database rows.
+	 *
+	 * @since 1.0.0
+	 */
+	private function reset_achievement_types() {
+		global $wpdb;
+
+		$achievement_types = badgeos_get_achievement_types_slugs();
+		$achievement_types[] = 'achievement-type';
+		$achievement_types[] = 'badgeos-log-entry';
+
+		//Post Types
+		$sql = "SELECT ID AS ID FROM {$wpdb->posts} WHERE post_type = %s";
+		$achievement_ids = array();
+		foreach( $achievement_types as $type ) {
+			$result_ids = $wpdb->get_results(
+				$wpdb->prepare(
+					$sql,
+					$type
+				)
+			);
+
+			if ( !empty( $result_ids ) ) {
+				foreach( $result_ids as $value ) {
+					$achievement_ids[] = absint( $value->ID );
+				}
+			}
+		}
+
+		//Attachments/media
+		$sql = "SELECT ID AS ID FROM {$wpdb->posts} WHERE post_type = %s AND post_parent = %d";
+		$attachment_ids = array();
+		foreach( $achievement_ids as $id ) {
+			$attachments_results_ids = $wpdb->get_results(
+				$wpdb->prepare(
+					$sql,
+					'attachment',
+					$id
+				)
+			);
+			if ( !empty( $attachments_results_ids ) ) {
+				foreach( $attachments_results_ids as $value ) {
+					$attachment_ids[] = absint( $value->ID );
+				}
+			}
+		}
+
+		foreach( $achievement_ids as $achievement ) {
+			wp_delete_post( $achievement );
+		}
+
+		foreach( $attachment_ids as $attachment ) {
+			wp_delete_attachment( $attachment );
+		}
+	}
+
+	/**
+	 * Run SQL DELETE statement on WordPress options table.
+	 *
+	 * @since 1.0.0
+	 */
+	private function reset_options() {
+		global $wpdb;
+
+		$sql = "DELETE FROM {$wpdb->options} WHERE option_name LIKE \"%s\"";
+
+		$wpdb->query(
+			$wpdb->prepare(
+				$sql,
+				'%badgeos%'
+			)
+		);
+
+	}
+
+	/**
+	 * Run SQL TRUNCATE statement on our p2p tables.
+	 * Truncate drops the table and then rebuilds it as new
+	 *
+	 * @since 1.0.0
+	 */
+	private function reset_p2p() {
+		global $wpdb;
+
+		$sqlp2p = "TRUNCATE TABLE {$wpdb->p2p}";
+
+		$wpdb->query(
+			$wpdb->prepare(
+				$sqlp2p,
+				''
+			)
+		);
+
+		$sqlp2pmeta = "TRUNCATE TABLE {$wpdb->p2pmeta}";
+
+		$wpdb->query(
+			$wpdb->prepare(
+				$sqlp2pmeta,
+				''
+			)
+		);
+	}
+
+	/**
+	 * Check if BadgeOS is available
+	 *
+	 * @since  1.0.0
+	 * @return bool True if BadgeOS is available, false otherwise
+	 */
+	public static function meets_requirements() {
+
+		if ( class_exists('BadgeOS') )
+			return true;
+		else
+			return false;
+
+	}
+
+	/**
+	 * Potentially output a custom error message and deactivate
+	 * this plugin, if we don't meet requriements.
+	 *
+	 * This fires on admin_notices.
+	 *
+	 * @since 1.0.0
+	 */
+	public function maybe_disable_plugin() {
+
+		if ( ! $this->meets_requirements() ) {
+			// Display our error
+			?>
+			<div id="message" class="error">
+			<p><?php printf( __( 'BadgeOS Reset requires BadgeOS and has been <a href="%s">deactivated</a>. Please install and activate BadgeOS and then reactivate this plugin.', 'badgeos-reset' ), admin_url( 'plugins.php' ) ); ?></p>
+			</div>
+
+			<?php
+			// Deactivate our plugin
+			deactivate_plugins( $this->basename );
+		}
+
+	}
+
+}
+
+$gameover = new BadgeOS_Reset();
---
+++ readme.txt
@@ -0,0 +1,56 @@
+=== BadgeOS Reset Developer Add-On ===
+Contributors: tw2113
+Tags: badge, badges, openbadges, learningtimes, credly, OBI, mozilla, open badges, achievement, award, reward, engagement, submission, nomination, API, open credit, credit, badgeOS
+Requires at least: 3.7
+Tested up to: 3.9.1
+Stable tag: 1.0.0
+License: GNU AGPLv3
+License URL: http://www.gnu.org/licenses/agpl-3.0.html
+
+BadgeOS Add-On to reset BadgeOS data in your database.
+
+== Description ==
+
+The "BadgeOS Reset Developer Add-On" is an extension that cleans up and removes as much BadgeOS data from the your database that it can accurately detect. This way, you can "start over" with a clean database while you are developing your achievement-based website or course.
+
+This is a plugin you should not leave active longer than you need, to prevent accidentally deleting your data when you do not intend to. Also, ALWAYS back up your database before use, just to be safe.
+
+Note: This Add-on requires the free BadgeOS plugin. [Get the BadgeOS plugin](http://wordpress.org/extend/plugins/badgeos/ "BadgeOS").
+
+= Resources / Helpful Links =
+
+BadgeOS Reset Developer Add-On is made available by Michael Beckwith based on the achievement awarding suite by [LearningTimes, LLC](http://www.learningtimes.com/ "LearningTimes web site"). Here are some ways to stay connected and to see what else LearningTimes is up to:
+
+* [BadgeOS.org](http://badgeos.org/ "BadgeOS web site") - Contact Us, Video Tutorials, Examples, News
+* [LearningTimes](http://www.learningtimes.com/ "LearningTimes web site") - Bringing people together through engaging online conferences, events and activities.
+* [Credly.com](https://credly.com/ "Credly web site") - Recognize, manage and share lifelong achievement
+* [Credly Knowledgebase](http://support.credly.com/ "Credly FAQ and Support") - Credly FAQ
+* [Facebook](https://www.facebook.com/LearningTimes "LearningTimes on Facebook") - BadgeOS and LearningTimes on Facebook
+* [Twitter](https://twitter.com/LearningTimes "LearningTimes on Facebook") - BadgeOS and LearningTimes Tweets
+* [E-Mailing List](http://badgeos.org/join-list/ "BadgeOS Occasional Mailing List - No spam!") - Occasional updates via e-mail
+* [GitHub](https://github.com/opencredit/badgeos "BadgeOS on GitHub") - Report issues, contribute code
+
+= License Info =
+
+Michael Beckwith licenses BadgeOS Reset Developer Add-On to you under the terms of the GNU Affero General Public License, version 3, as published by the Free Software Foundation.
+
+There is NO WARRANTY for this software, express or implied, including the implied warranties of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License, version 3, at [http://www.gnu.org/licenses/agpl-3.0.html](http://www.gnu.org/licenses/agpl-3.0.html "License") for more details.
+
+== Installation ==
+
+1. First get and activate the free [BadgeOS plugin](http://wordpress.org/extend/plugins/badgeos/ "BadgeOS").
+2. Second, click on "Add New" in your Plugin Admin area in your WordPress Admin panel.
+3. Search for "BadgeOS Reset" and click "Install Now".
+4. Visit the BadgeOS settings page to check the appropriate setting and hit "Save Settings"
+
+
+== Changelog ==
+
+= 1.0.0 =
+* Intial release
+
+
+== Upgrade Notice ==
+
+= 1.0.0 =
+* Intial release
